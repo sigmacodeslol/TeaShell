d6627b4e54bcf0a4c7bf557c637120c6b43ace2a
sigmacodeslol <m96153045@gmail.com>
2025-03-08 12:04:32 +0000

    made log file name generator

--- teashell/logger.py
+++ teashell/logger.py
@@ -6,7 +6,7 @@ from grhh import grhh
 class _LOG:
     def __init__(self) -> None:
         now: str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        mod_strf: dict = {"now": now, "rhh": grhh(2), "ftype": "log"}
+        mod_strf: dict = {"now": now, "rhh": grhh(8), "ftype": "log"}
         self._logf: str = "%(now)s+%(rhh)s.%(ftype)s" % mod_strf
         print(self._logf)
 
