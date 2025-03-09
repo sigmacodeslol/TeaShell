58361d1a443174f208efc829602018b77816b8e3
sigmacodeslol <m96153045@gmail.com>
2025-03-09 17:27:02 +0000

    fixed parent folder problem from import

--- teashell/config.py
+++ teashell/config.py
@@ -1,8 +1,11 @@
 import yaml
+from pathlib import Path
 
 
 def load_config() -> __import__("typing").Any:
-    with open("../config.yaml", "r") as f:
+    current_dir = Path(__file__).parent
+    config_yaml = str(current_dir.parent / "config.yaml")
+    with open(config_yaml, "r") as f:
         return yaml.safe_load(f)
 
 
