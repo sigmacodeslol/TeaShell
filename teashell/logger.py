0408dded586b8479721359c70d072dd535587d06
sigmacodeslol <m96153045@gmail.com>
2025-03-09 17:44:14 +0000

    empty logs dir if at max_logs threshold

--- teashell/logger.py
+++ teashell/logger.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from _err import Terminate
 from config import config
 from grhh import grhh
+from scripts.cleanup_logs import cleanup
 
 
 @dataclass
@@ -24,7 +25,7 @@ class _LOG:
         self._logf: str = "%(now)s+%(rhh)s.%(ftype)s" % mod_strf
 
         current_dir = Path(__file__).parent
-        log_dir = current_dir.parent / "logs"
+        self._log_dir = current_dir.parent / "logs"
         self._logfp = str(log_dir / self._logf)
 
     def __call__(self, mode: LWrite):
@@ -34,6 +35,12 @@ class _LOG:
             raise Terminate("Invalid mode for _LOG")
 
     def _write(self, *args, **kwargs):
+        # empty the log folder if it has more than the max logs attr in config.yaml
+        folder = self._logf
+        num_files = sum(1 for entry in os.scandir(folder) if entry.is_file())
+        if num_files >= config["logging"]["max_logs"]:
+            cleanup(self._log_dir)
+
         lw: LWrite = args[0]
         if Path(self._logfp).exists():
             with open(self._logfp, "a") as logf:
