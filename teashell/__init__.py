4845e283b73b21c7b928a7229b5af173c6301da9
sigmacodeslol <m96153045@gmail.com>
2025-03-09 17:23:51 +0000

    added logging functionality from logger.py

--- teashell/__init__.py
+++ teashell/__init__.py
@@ -1,3 +1,22 @@
 import logger
+from logger import LWrite
 
-logger._LOG()
+# print(logger._LOG()(LWrite("msys", "initialized sys global vars", "init")))
+
+
+class TeaShell:
+    def __init__(self) -> None:
+        self.__log = logger._LOG()
+
+    def __call__(self) -> None:
+        log = self.__log
+        log(LWrite("msys", "initialized sys global vars", "init"))
+        log(LWrite("msys", "terminated shell", "exit"))
+
+
+tsh = (
+    TeaShell()()
+)  # when finished project delete second pair of brackets and write the main.py in parent folder.
+
+
+__all__ = ["tsh", "TeaShell"]
