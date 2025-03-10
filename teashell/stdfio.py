961a13163a7ff7469e325322d5836495506887e5
sigmacodeslol <m96153045@gmail.com>
2025-03-10 18:20:48 +0000

    _write and _append func

--- teashell/stdfio.py
+++ teashell/stdfio.py
@@ -7,5 +7,11 @@ class _Path:
     def __init__(self, path: Path) -> None:
         self.__p: Path = path
 
+    def _write(self, content: str) -> None:
+        self.__p.write_text(content)
+
+    def _append(self, content: str) -> None:
+        self.__p.open("a").write(content)
+
 
 __all__ = ["_Path"]
