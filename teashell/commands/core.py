66bca2cb883cd6385e81b5695a90a3acbe7004b8
sigmacodeslol <m96153045@gmail.com>
2025-03-15 13:21:03 +0000

    longest(arr) and updated cmds type hinting in _help.__init__

--- teashell/commands/core.py
+++ teashell/commands/core.py
@@ -1,9 +1,14 @@
-from typing import Callable
+from typing import Any, Callable
 
+def longest(arr: Any):
+    if not arr:
+        return None
+    
+    return max(arr, key=len)
 
 class _help:
-    def __init__(self, cmds: dict[str, Callable]) -> None:
-        self.cmds: dict[str, Callable] = cmds
+    def __init__(self, cmds: dict[str, dict[Callable, str]]) -> None:
+        self.cmds: dict[str, dict[Callable, str]] = cmds
 
     def __call__(self) -> None:
         print("Commands:")
