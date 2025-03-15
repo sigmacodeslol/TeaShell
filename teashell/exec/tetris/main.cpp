155405be268498fd0b196d9699dd08954f31a118
sigmacodeslol <m96153045@gmail.com>
2025-03-15 12:59:52 +0000

    unfinished

---
+++ teashell/exec/tetris/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+wstring tetromino[7];
+
+int main() {
+  // Create assets
+  tetromino[0].append(L"..X.");
+  tetromino[0].append(L"..X.");
+  tetromino[0].append(L"..X.");
+  tetromino[0].append(L"..X.");
+}
