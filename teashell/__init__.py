fca78c108fce8e4f6d662f459ccd5605cb0b34e7
sigmacodeslol <m96153045@gmail.com>
2025-03-15 13:00:52 +0000

    tuple return for numpass and numfail

--- teashell/__init__.py
+++ teashell/__init__.py
@@ -1,29 +1,53 @@
 from typing import Callable
 
+import art
+from commands import *
 from config import config
 import logger
 from logger import LWrite
 
 
+class Cmd:
+    def __init__(self, f: Callable) -> None:
+        self.f: Callable = f
+
+    def __call__(self, *args, **kwargs) -> None:
+        self.f(*args, **kwargs)
+
+
 class _CMDdb:
-    def __init__(self) -> None:
-        self.__cmds: dict = {}
+    def __init__(self, _log: logger._LOG) -> None:
+        self._cmds: dict = {}
+        self._log: logger._LOG = _log
 
     def register(self, cmd: str, func: Callable) -> None:
-        self.__cmds[cmd]: Callable = func
+        self._cmds[cmd]: Callable = func
 
     def remove(self, cmd: str) -> None:
-        self.__cmds.pop(cmd, None)
+        self._cmds.pop(cmd, None)
+
+    def get(self, cmd: str) -> Callable:
+        return self._cmds.get(cmd, None)
 
 
 class TeaShell:
     def __init__(self) -> None:
         self.__log = logger._LOG(config)
 
+        self.__cmds = _CMDdb(self.__log)
+        # cmd registers
+        self.__cmds.register("help", core._help(self.__cmds._cmds))
+
     def __call__(self) -> None:
         log = self.__log
+
         log(LWrite("msys", "initialized sys global vars", "init"))  # msys: main sys
-        log(LWrite("msys", "successfully printed teashell logo", "init"))
+
+        check_errors = art.format("TeaSHell")
+        if(check_errors[1] > 0):
+            log(LWrite("msys", "failed to print teashell logo", "init"))
+        else:
+            log(LWrite("msys", "successfully printed teashell logo", "init"))
 
         log(LWrite("msys", "terminated shell", "exit"))
 
--- teashell/art.py
+++ teashell/art.py
@@ -1,5 +1,27 @@
 import pyfiglet
+import sys
+from typing import Tuple
 
 
-def format(text: str, font: str = "lean"):
-    return pyfiglet.figlet_format(text, font)
+def format(text: str, font: str = "lean", *, file=sys.stdout.write) -> Tuple[int, int]:
+    """
+    ``` python
+    format() -> (numpass, numfail)
+    ```
+
+    Args:
+        text (str): text to format
+        font (str, optional): font for text format. Defaults to "lean".
+        file (_type_, optional): tool to write to. Defaults to sys.stdout.write.
+
+    Returns:
+        Tuple[int, int]: (numpass, numfail)
+    """
+    try:
+        file(pyfiglet.figlet_format(text, font))
+        return (1, 0)
+    except:
+        return (0, 1)
+
+
+__all__ = ["format"]
