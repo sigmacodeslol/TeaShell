0454621412a5bc56fe1359ae506e6bf8b619f067
sigmacodeslol <m96153045@gmail.com>
2025-03-15 13:07:47 +0000

    default font

--- teashell/art.py
+++ teashell/art.py
@@ -3,7 +3,7 @@ import sys
 from typing import Tuple
 
 
-def format(text: str, font: str = "lean", *, file=sys.stdout.write) -> Tuple[int, int]:
+def format(text:str, font:str|None="lean", *, file=sys.stdout.write) -> Tuple[int, int]:
     """
     ``` python
     format() -> (numpass, numfail)
@@ -11,14 +11,16 @@ def format(text: str, font: str = "lean", *, file=sys.stdout.write) -> Tuple[int
 
     Args:
         text (str): text to format
-        font (str, optional): font for text format. Defaults to "lean".
-        file (_type_, optional): tool to write to. Defaults to sys.stdout.write.
+        font (str, None, optional): font for text format. Defaults to "lean".
+        file (optional): tool to write to. Defaults to sys.stdout.write.
 
     Returns:
         Tuple[int, int]: (numpass, numfail)
     """
     try:
-        file(pyfiglet.figlet_format(text, font))
+        file(pyfiglet.figlet_format(text)
+             if(font is None)
+             else pyfiglet.figlet_format(text, font))
         return (1, 0)
     except:
         return (0, 1)
